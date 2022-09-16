47e5389aaa6b933b6d5ca7bc2500643bf88b8f8c
Reuben-PC <2824016820@qq.com>
2022-09-16 15:17:15 +0800

    unity

---
+++ RenderGraph/Assets/ReubenRenderPipeline.BasePass.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
+using UnityEngine.Rendering;
+
+public partial class ReubenRenderPipeline_BasePass
+{
+    private ShaderTagId _passName = new ShaderTagId("BasePass");
+    
+    public class BasePassData
+    {
+        public RendererListHandle _renderList_Qpaque;
+        public RendererListHandle _renderList_Transparent;
+        public TextureHandle _Albedo;
+        public TextureHandle _Emission;
+        public TextureHandle _Depth;
+    }
+    
+    private TextureHandle CreateColorTexture(RenderGraph graph, Camera camera, string name)
+    {
+        bool colorRT_sRGB = (QualitySettings.activeColorSpace == ColorSpace.Linear);
+
+        //Texture description
+        TextureDesc colorRTDesc = new TextureDesc(camera.pixelWidth, camera.pixelHeight);
+        colorRTDesc.colorFormat = GraphicsFormatUtility.GetGraphicsFormat(RenderTextureFormat.Default,colorRT_sRGB);
+        colorRTDesc.depthBufferBits = 0;
+        colorRTDesc.msaaSamples = MSAASamples.None;
+        colorRTDesc.enableRandomWrite = false;
+        colorRTDesc.clearBuffer = true;
+        colorRTDesc.clearColor = Color.black;
+        colorRTDesc.name = name;
+
+        return graph.CreateTexture(colorRTDesc);
+    }
+
+    private TextureHandle CreateDepthTexture(RenderGraph graph, Camera camera)
+    {
+        bool colorRT_sRGB = (QualitySettings.activeColorSpace == ColorSpace.Linear);
+
+        //Texture description
+        TextureDesc colorRTDesc = new TextureDesc(camera.pixelWidth, camera.pixelHeight);
+        colorRTDesc.colorFormat = GraphicsFormatUtility.GetGraphicsFormat(RenderTextureFormat.Depth,colorRT_sRGB);
+        colorRTDesc.depthBufferBits = DepthBits.Depth24;
+        colorRTDesc.msaaSamples = MSAASamples.None;
+        colorRTDesc.enableRandomWrite = false;
+        colorRTDesc.clearBuffer = true;
+        colorRTDesc.clearColor = Color.black;
+        colorRTDesc.name = "Depth";
+
+        return graph.CreateTexture(colorRTDesc);
+    }
+}
---
+++ RenderGraph/Assets/ReubenRenderPipeline.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
+using UnityEngine.Rendering;
+
+public partial class ReubenRenderPipeline : RenderPipeline
+{
+    private readonly string cmdName = "Reuben Render Graph";
+    #region RenderGraph
+
+    private RenderGraph _RenderGraph;
+
+    void InitRenderGraph()
+    {
+        _RenderGraph = new RenderGraph("Reuben Render Graph");
+    }
+
+    void CleanupRenderGraph()
+    {
+        _RenderGraph.Cleanup();
+        _RenderGraph = null;
+    }
+
+    #endregion
+    
+    
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(context, cameras);
+        InitRenderGraph();
+        RenderGamera(context, cameras[0]);
+        _RenderGraph.EndFrame();
+        EndFrameRendering(context, cameras);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        CleanupRenderGraph();
+    }
+
+    void RenderGamera(ScriptableRenderContext context, Camera mainCamera)
+    {
+        BeginCameraRendering(context, mainCamera);
+
+        ScriptableCullingParameters cullingParams;
+        if(!mainCamera.TryGetCullingParameters(out cullingParams)) return;
+        CullingResults cull = context.Cull(ref cullingParams);
+
+        context.SetupCameraProperties(mainCamera);
+        
+        CommandBuffer cmd = CommandBufferPool.Get(cmdName);
+        RenderGraphParameters rgParams = new RenderGraphParameters()
+        {
+            executionName = "Reuben Render Graph Excute",
+            commandBuffer = cmd,
+            scriptableRenderContext = context,
+            currentFrameIndex = Time.frameCount
+        };
+
+        using (_RenderGraph.RecordAndExecute(rgParams))
+        {
+            
+        }
+        
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+        context.Submit();
+        EndCameraRendering(context, mainCamera);
+    }
+}
